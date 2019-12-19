eb85ad6f3fcc5693d9e451d06f0c3fa3c2844022
yasukotelin <yasukotelin@gmail.com>
2019-12-19 13:19:10 +0900

    Update so that you can continue when leave from the diff.

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/gookit/color"
 	"github.com/urfave/cli"
 	"github.com/yasukotelin/git-diffs/git"
-	"github.com/gookit/color"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "git-diffs"
-	app.Version = "1.3.1"
+	app.Version = "1.4.0"
 	app.Description = "The git subcommand that is diff files selector."
 	app.Action = mainAction
 
@@ -37,6 +37,21 @@ func mainAction(c *cli.Context) error {
 		return err
 	}
 
+	for {
+		isContinue, err := askToSelectFile(stagedFiles, unstagedFiles)
+		if err != nil {
+			return err
+		}
+		if !isContinue {
+			break
+		}
+		fmt.Println()
+	}
+
+	return nil
+}
+
+func askToSelectFile(stagedFiles []git.DiffFile, unstagedFiles []git.DiffFile) (isContinue bool, err error) {
 	fmt.Println("Staged files:")
 	fmt.Println()
 	stagedFilesLen := len(stagedFiles)
@@ -59,26 +74,27 @@ func mainAction(c *cli.Context) error {
 	}
 	fmt.Println()
 
-	if (stagedFilesLen + unstagedFilesLen == 0) {
-		return nil
+	if stagedFilesLen+unstagedFilesLen == 0 {
+		return false, nil
 	}
-	
+
 	fmt.Print("Select number (empty is cancel) => ")
 
 	var selNumStr string
 	fmt.Scanln(&selNumStr)
 
 	if selNumStr == "" {
-		return nil
+		return false, nil
 	}
 	selNum, err := strconv.Atoi(selNumStr)
 	if err != nil {
-		return errors.New("your input is not number.")
+		return false, errors.New("your input is not number.")
 	}
 	if selNum > stagedFilesLen+unstagedFilesLen || selNum < 1 {
-		return errors.New("your input is out of range numbers")
+		return false, errors.New("your input is out of range numbers")
 	}
 
+	fmt.Println()
 	if selNum <= stagedFilesLen {
 		// User selected staged file number
 		err = git.Diff(stagedFiles[selNum-1].Path, true)
@@ -87,8 +103,8 @@ func mainAction(c *cli.Context) error {
 		err = git.Diff(unstagedFiles[selNum-stagedFilesLen-1].Path, false)
 	}
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	return nil
+	return true, nil
 }
