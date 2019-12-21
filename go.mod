02c6001bc99c6e24501e65bdf31228b1d9027131
yasukotelin <yasukotelin@gmail.com>
2019-12-22 00:58:05 +0900

    Refactor the code

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gookit/color v1.2.0
 	github.com/urfave/cli v1.22.1
 	github.com/yasukotelin/gitlib v0.0.0-20191219141545-28ddd4ccc329
+	github.com/yasukotelin/scrlib v0.0.0-20190218052844-4051981f446c
 )
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "git-diffs"
-	app.Version = "1.4.2"
+	app.Version = "1.5.0"
 	app.Description = "The git subcommand that is diff files selector."
 	app.Action = mainAction
 
@@ -38,73 +38,87 @@ func mainAction(c *cli.Context) error {
 	}
 
 	for {
-		isContinue, err := askToSelectFile(stagedFiles, unstagedFiles)
+		printFiles(stagedFiles, unstagedFiles)
+		totalLen := len(stagedFiles) + len(unstagedFiles)
+		if totalLen == 0 {
+			return nil
+		}
+
+		selNum, err := askToSelectFile(stagedFiles, unstagedFiles)
 		if err != nil {
 			return err
 		}
-		if !isContinue {
+		if selNum == 0 {
 			break
 		}
 		fmt.Println()
+		err = runDiff(selNum, stagedFiles, unstagedFiles)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func askToSelectFile(stagedFiles []gitlib.DiffFile, unstagedFiles []gitlib.DiffFile) (isContinue bool, err error) {
+func printFiles(staged, unstaged []gitlib.DiffFile) {
+	// print steged files
 	fmt.Println("Staged files:")
 	fmt.Println()
-	stagedFilesLen := len(stagedFiles)
+	stagedFilesLen := len(staged)
 	if stagedFilesLen == 0 {
 		fmt.Println("\tno staged files.")
 	}
-	for i, file := range stagedFiles {
+	for i, file := range staged {
 		color.Green.Printf("\t[%d]\t%v\t%v\n", i+1, file.Status, file.Path)
 	}
 	fmt.Println()
 
+	// print unstaged files
 	fmt.Println("Unstaged files:")
 	fmt.Println()
-	unstagedFilesLen := len(unstagedFiles)
+	unstagedFilesLen := len(unstaged)
 	if unstagedFilesLen == 0 {
 		fmt.Println("\tno unstaged files.")
 	}
-	for i, file := range unstagedFiles {
+	for i, file := range unstaged {
 		color.Red.Printf("\t[%d]\t%v\t%v\n", i+1+stagedFilesLen, file.Status, file.Path)
 	}
 	fmt.Println()
+}
 
-	if stagedFilesLen+unstagedFilesLen == 0 {
-		return false, nil
-	}
-
+func askToSelectFile(stagedFiles []gitlib.DiffFile, unstagedFiles []gitlib.DiffFile) (int, error) {
 	fmt.Print("Select number (empty is cancel) => ")
 
 	var selNumStr string
 	fmt.Scanln(&selNumStr)
 
 	if selNumStr == "" {
-		return false, nil
+		return 0, nil
 	}
 	selNum, err := strconv.Atoi(selNumStr)
 	if err != nil {
-		return false, errors.New("your input is not number")
+		return 0, errors.New("your input is not number")
 	}
-	if selNum > stagedFilesLen+unstagedFilesLen || selNum < 1 {
-		return false, errors.New("your input is out of range numbers")
+	if selNum > len(stagedFiles)+len(unstagedFiles) || selNum < 1 {
+		return 0, errors.New("your input is out of range numbers")
 	}
 
-	fmt.Println()
-	if selNum <= stagedFilesLen {
+	return selNum, nil
+}
+
+func runDiff(number int, stagedFiles, unstagedFiles []gitlib.DiffFile) error {
+	var err error
+	if number <= len(stagedFiles) {
 		// User selected staged file number
-		err = gitlib.RunDiff(stagedFiles[selNum-1].Path, true)
+		err = gitlib.RunDiff(stagedFiles[number-1].Path, true)
 	} else {
 		// User selected unstaged file number
-		err = gitlib.RunDiff(unstagedFiles[selNum-stagedFilesLen-1].Path, false)
+		err = gitlib.RunDiff(unstagedFiles[number-len(stagedFiles)-1].Path, false)
 	}
 	if err != nil {
-		return false, err
+		return err
 	}
 
-	return true, nil
+	return nil
 }
